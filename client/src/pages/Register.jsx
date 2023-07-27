259a5c4854ab89b62ae852b9b891a79e5b3650cb
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-27 19:49:32 +0530

    Validation

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -13,19 +13,42 @@ const Register = () => {
     confirmPassword: "",
   });
 
-  const handleValidation = () => {
-    const { password, confirmPassword } = values;
-
-    if (password != confirmPassword) {
-      toast.error("Passwords do not match.", {
-        position: "bottom-right",
-        autoClose: 8000,
-        pauseOnHover: true,
-        draggable: true,
-        theme: "dark",
-      });
-    }
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
   };
+
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
+
+    return true;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     handleValidation();
