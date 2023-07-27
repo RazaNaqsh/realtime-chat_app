21e38ae565e6bb5a72f72692293c91750b9bd92d
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-27 19:17:28 +0530

    Routes and respective components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
 const App = () => {
   return (
     <>
-      <h1>Hey this is fresh vite stuff</h1>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
     </>
   );
 };
