99270f12ce5634e0c9b34399d89fb5c42d5c9bd8
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-29 12:34:54 +0530

    Avatar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
+import SetAvatar from "./pages/SetAvatar";
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path="/" element={<Chat />} />
       </Routes>
     </>
